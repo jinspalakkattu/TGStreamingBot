0670203c40fed9da2aeab6ba60be13c822a90e98
Jins Mathew <jins.palakkattu@gmail.com>
2021-09-15 16:28:58 +0300

    [Updates] 👑 Major Update Added Audio/Video Playback Buttons Change Library pytgcalls To py-tgcalls Added Youtube Search Option

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "[INFO] - FETCHING UPSTREAM REPO ..."
 git clone https://github.com/jinspalakkattu/TGStreamingBot /TGStreamingBot
-cd /TGStreamingBot
+#cd /TGStreamingBot
 echo "[INFO] - INSTALLING REQUIREMENTS ..."
 pip3 install -U -r requirements.txt
 echo "[INFO] - STARTED VIDEO PLAYER BOT ..."
